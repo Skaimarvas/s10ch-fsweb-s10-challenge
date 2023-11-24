b9218faa1cb342916825ed943e1972d924977b75
skaimarvas <skaimarvas@gmail.com>
2023-11-24 15:23:43 +0300

    redux-logger yüklendi ve store oluşturuldu

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -11,17 +11,20 @@ export function notEkle(not) {
 export function notSil(notId) {
   return { type: NOT_SIL, payload: notId };
 }
-
-export const notEkleAPI = (yeniNot) => (dispatch) => {
-  axios
-    .post("https://httpbin.org/anything", yeniNot)
-    .then((res) => {
-      if (res.status === 200) {
-        // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notEkle ile dispatch edin
-      }
-    })
-    .catch((error) => console.log(error));
-};
+//redux thunk action
+export const notEkleAPI =
+  (yeniNot) =>
+  //redux thunk function
+  (dispatch) => {
+    axios
+      .post("https://httpbin.org/anything", yeniNot)
+      .then((res) => {
+        if (res.status === 200) {
+          // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notEkle ile dispatch edin
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
 export const notSilAPI = (id) => (dispatch) => {
   console.log(id);
--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,15 @@
 import { reducer } from "./reducers/reducers";
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+
+//unutmamak adına combinereducers ekledim
+
+// const  comReducer = combineReducers({
+//     deneme: deneme,
+//     deneme1: deneme1,
+// })
+
+export const store = createStore(reducer, applyMiddleware(thunk));
